154223d448d3c974a3fa54f09b6d05bee91918bb
Setthapon Thadisakun <steam.reaw1@gmail.com>
2022-11-17 14:55:28 +0700

    update attributes

--- playlist.py
+++ playlist.py
@@ -1,6 +1,8 @@
 from song import Song
 
+
 class Playlist:
 
-    def __init__(self):
-        pass
+    def __init__(self, name: str, song: ["Song"]):
+        self.__name = name
+        self.__song = song
