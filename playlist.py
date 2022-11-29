1c4720f9a5a53ca10b2a88a55fbbde00df0ab536
Setthapon Thadisakun <steam.reaw1@gmail.com>
2022-11-29 23:43:59 +0700

    add auto add and play a song method

--- playlist.py
+++ playlist.py
@@ -1,16 +1,19 @@
+from youtube_search import YoutubeSearch
+import random
+import webbrowser
 from song import Song
 from playlist_database import Playlistdb
 from display import Display
-from youtube_search import YoutubeSearch
-import random
 
 
 class Playlist:
 
-    def __init__(self, name: str, song: ["Song"], database: "Playlistdb"):
+    def __init__(self, name: str, song: ["Song"], database: "Playlistdb",
+                 display: "Display" = Display()):
         self.__name = name
         self.__song = song
         self.__database = database
+        self.__display = display
         database.initialize(self)
 
     @property
@@ -34,48 +37,37 @@ class Playlist:
             song_name = input("Please input song name: ")
             if isinstance(song_name, str) and song_name != "":
                 break
-            else:
-                print("Song's name must be string")
-                continue
+            print("Song's name must be string")
+            continue
         while True:
             artist = input("Please input artist : ")
             if isinstance(artist, str) and artist != "":
                 break
-            else:
-                print("Artist's name must be string")
-                continue
+            print("Artist's name must be string")
+            continue
         while True:
-            duration = input("Please input song name ex:(3:21): ")
+            duration = input("Please input song name EX(3:21): ")
             if ":" not in duration or not isinstance(duration, str):
                 print("Incorrect duration format, Please Try again")
                 continue
-            else:
-                break
+            break
         while True:
             language = input("Please input song language (not required): ")
             if isinstance(language, str):
                 if language == "":
                     language = "None"
                     break
-            else:
-                print("Language must be string, Please Try again")
-                continue
+            print("Language must be string, Please Try again")
+            continue
         while True:
             url = input("Please input song url (not required): ")
             if isinstance(url, str):
                 if url == "":
                     url = "None"
                     break
-            else:
-                print("url must be string")
-                continue
+            print("url must be string")
+            continue
         new_song = Song(song_name, artist, duration, language, url)
-        print("Please confirm song information")
-        print(f"Song's name: {new_song.name}")
-        print(f"artist's name: {new_song.artist}")
-        print(f"Song's duration: {new_song.duration}")
-        print(f"Song's language: {new_song.name}")
-        print(f"Song's url: {new_song.url}")
         user_input = input("Press enter to confirm: ")
         if user_input == "":
             self.__song.append(new_song)
@@ -83,12 +75,30 @@ class Playlist:
             print(f"{new_song.name} is added to {self.__name}")
         else:
             print(f"{new_song.name} isn't added to {self.__name}")
-            pass
 
     def auto_add_song(self):
-        user_input = input("Please input song's name: ")
+        while True:
+            user_input = input("Please input song's name: ")
+            if user_input == "":
+                print("Please input song's name properly")
+                continue
+            break
         results = YoutubeSearch(user_input, max_results=3).to_dict()
-        Display().draw_songs_table_youtube(results)
+        self.__display.draw_songs_table_youtube(results)
+        while True:
+            try:
+                user_input = int(input("Please choose wanted song's number: "))
+                if not 0 < user_input < 4:
+                    raise ValueError
+            except (ValueError, TypeError):
+                print("Song's number must be int and between 1 to 3")
+                continue
+            break
+        new_song = Song(results[user_input - 1]["title"],
+                        results[user_input - 1]["channel"],
+                        results[user_input - 1]["duration"])
+        self.__song.append(new_song)
+        self.__database.initialize(self)
 
     def delete_all_songs(self):
         self.__song = []
@@ -98,3 +108,14 @@ class Playlist:
     def shuffle_songs(self):
         random.shuffle(self.__song)
         self.__database.initialize(self)
+
+    def play_a_song(self):
+        self.__display.draw_songs_table(self.__song)
+        num_songs = len(self.__song)
+        while True:
+            user_input = int(input("PLease choose wanted song: "))
+            if 0 < user_input < num_songs:
+                print("Please input correct song's number")
+                break
+        url = self.__song[user_input - 1].url
+        webbrowser.open(url)
