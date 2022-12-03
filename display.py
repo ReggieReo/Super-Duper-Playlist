135ee308af3441661222589d445c60ee0ed62095
Setthapon Thadisakun <steam.reaw1@gmail.com>
2022-12-03 23:49:05 +0700

    add draw menu after open playlist

--- display.py
+++ display.py
@@ -52,6 +52,42 @@ class Display:
         print(table)
 
     @staticmethod
+    def draw_opened_menu():
+        table = PrettyTable()
+        table.field_names = ["#", "Function"]
+        table.add_rows([["1", "Play a song"],
+                        ["2", "Add a song"],
+                        ["3", "Delete songs"],
+                        ["4", "Edit song's information"],
+                        ["5", "Get song's information"],
+                        ["6", "Close this playlist"]])
+        table.align["Function"] = "l"
+        print(table)
+
+    @staticmethod
+    def draw_add_song():
+        table = PrettyTable()
+        table.field_names = ["#", "Function"]
+        table.add_rows([["1", "Add a song"],
+                        ["2", "Auto add a song"]])
+        table.align["Function"] = "l"
+        print(table)
+
+    @staticmethod
+    def edit_song_menu():
+        table = PrettyTable()
+        table.field_names = ["#", "Function"]
+        table.add_rows([["1", "Edit song's name"],
+                        ["2", "Edit song's artist"],
+                        ["3", "Edit song's duration"],
+                        ["4", "Edit song's language"],
+                        ["5", "Edit song's url"],
+                        ["6", "Edit song's rating"],
+                        ["7", "Stop editing"]])
+        table.align["Function"] = "l"
+        print(table)
+
+    @staticmethod
     def clear_screen():
         if name == 'nt':
             # for windows
